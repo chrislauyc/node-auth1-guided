1e44efb349ed910dd1cdb30b15eff5dc7b95d81a
Chris Lau <chrislyc.lau@gmail.com>
2021-08-31 19:17:01 -0600

    authrouter register

--- api/server.js
+++ api/server.js
@@ -1,10 +1,26 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
-
+const session = require("express-session");
 const usersRouter = require('./users/users-router.js');
-
+const authRouter = require("./auth/auth-router")
 const server = express();
+
+const config = {
+  name:"sessionId",
+  secret: "keep it secret, keep it safe",
+  cookie:{
+    maxAge: 1000 * 60 * 60,
+    secure:false,
+    httpOnly: true
+  },
+  resave:false,
+  saveUnitialized:false
+}
+
+server.use(session(config));
+
+
 
 server.use(express.static(path.join(__dirname, '../client')));
 server.use(helmet());
@@ -12,9 +28,12 @@ server.use(express.json());
 
 server.use('/api/users', usersRouter);
 
+server.use("/api/auth",authRouter);
+
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
+
 
 server.use('*', (req, res, next) => {
   next({ status: 404, message: 'not found!' });
