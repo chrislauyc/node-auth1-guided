865bef0c1a645609eb9de29845d0ecba5063f355
Dave Kidd <dave@davekidd.net>
2021-08-31 19:18:43 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res, next) => {
+router.get("/",restricted, (req, res, next) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
