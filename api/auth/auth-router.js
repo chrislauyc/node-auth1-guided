3cbff3081ef1e446b60b29f221211c5462bd53de
Dave Kidd <dave@davekidd.net>
2021-08-31 18:17:37 -0600

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -12,7 +12,7 @@ const checkPayload = (req,res,next)=>{
 }
 const checkUserInDB = async (req,res,next)=>{
     try{
-
+        const user = await User.findBy({username:req.body.username})
     }catch(e){
         res.status(500).json(`Server error: ${e}`)
     }
