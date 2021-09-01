60f0924caa3a4fcafd4770609c551064de4cadef
Dave Kidd <dave@davekidd.net>
2021-08-31 19:19:47 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
+const restricted = (req,res,next)=>{
+  if(req.session && req.session.user){
+    next()
+  }else{
+    res.status(401).json("Unauthorized")
+  }
+}
+
 router.get("/",restricted, (req, res, next) => {
   Users.find()
     .then(users => {
